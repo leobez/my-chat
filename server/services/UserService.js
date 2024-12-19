2394a86fcabe9efed60a9ebfc703503b59749419
leobez <leossbezerra@gmail.com>
2024-12-19 12:59:01 -0300

    updated todo

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -201,7 +201,6 @@ class UserService {
             return true
 
         } catch (error) {
-            console.log(error)
             if (error.type === 'model') {
                 // Add error logger here
                 throw new CustomError(500, 'Server error', ['Try again later'])

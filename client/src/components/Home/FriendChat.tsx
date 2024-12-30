dcc74f7ebc78d333a54b6e5b28c3daa43b24d92b
leobez <leossbezerra@gmail.com>
2024-12-29 23:15:12 -0300

    updated todo

--- client/src/components/Home/FriendChat.tsx
+++ client/src/components/Home/FriendChat.tsx
@@ -19,6 +19,7 @@ const FriendChat = (props: Props) => {
     const me_id = useSelector((state:any) => state.auth.userId)
     const friend_id = props.userId
 
+
     // Friend data
     const friends = useSelector((state:any) => state.friendship.friends)
     const [friend, setFriend] = useState<Friend|null>(null)
@@ -31,8 +32,6 @@ const FriendChat = (props: Props) => {
 
     useEffect(() => {
 
-        console.log('friends: ', friend)
-
         friends.forEach((friend:Friend) => {
             if (friend.userId === friend_id) {
                 setFriend(friend)
@@ -41,7 +40,7 @@ const FriendChat = (props: Props) => {
 
         getHistoryWithUser(friend_id);
 
-    }, [friends])
+    }, [friends, friend_id])
 
     const allHistories = useSelector((state:any) => state.message.completeHistory)
     const [messages, setMessages] = useState<Message[]>([])
@@ -59,6 +58,7 @@ const FriendChat = (props: Props) => {
     // Auto scroll to bottom of chat box when history updates
     const endOfChatBoxRef = useRef<any>(null)
     useEffect(() => {
+        console.log('messages: ', messages)
         if (endOfChatBoxRef.current) {
             endOfChatBoxRef.current.scrollIntoView({ behavior: 'smooth' });
         }
@@ -85,107 +85,114 @@ const FriendChat = (props: Props) => {
     const {deleteFriendship} = useDeleteFriendship()
     const handleDelete = (friendshipId:number) => {
         deleteFriendship(friendshipId)
+        setFriend(null)
     }
 
     return (
-        <div className="w-full h-full">
-           {friend && 
-                <div className="flex flex-col h-full">
+        <>
+            {friend && 
 
-                    <div className="h-16 bg-black text-white font-bold flex justify-between items-center px-3">
+                <div className="w-full h-full">
 
-                        <div className="flex items-center justify-start gap-2">
-                            <p className="text-white">{friend.username}</p>
-                            <p className="text-white text-sm">#{friend.userId}</p>
-                            <div>
-                                {
-                                    friend.online ? (
-                                        <div className="flex gap-2 items-center justify-center">
-                                            <div className="h-4 w-4 rounded-full border-2 border-green-600 p-[2px]">
-                                                <div className="h-full w-full rounded-full bg-green-600"/>
+                    <div className="flex flex-col h-full">
+    
+                        <div className="h-16 bg-black text-white font-bold flex justify-between items-center px-3">
+    
+                            <div className="flex items-center justify-start gap-2">
+                                <p className="text-white">{friend.username}</p>
+                                <p className="text-white text-sm">#{friend.userId}</p>
+                                <div>
+                                    {
+                                        friend.online ? (
+                                            <div className="flex gap-2 items-center justify-center">
+                                                <div className="h-4 w-4 rounded-full border-2 border-green-600 p-[2px]">
+                                                    <div className="h-full w-full rounded-full bg-green-600"/>
+                                                </div>
+                                                <p className="text-xs font-thin text-green-600">Online</p>
                                             </div>
-                                            <p className="text-xs font-thin text-green-600">Online</p>
-                                        </div>
-                                    ) : (
-                                        <div className="flex gap-2 items-center justify-center">
-                                            <div className="h-4 w-4 rounded-full border-2 border-red-600 p-[2px]">
-                                                <div className="h-full w-full rounded-full bg-red-600"/>
+                                        ) : (
+                                            <div className="flex gap-2 items-center justify-center">
+                                                <div className="h-4 w-4 rounded-full border-2 border-red-600 p-[2px]">
+                                                    <div className="h-full w-full rounded-full bg-red-600"/>
+                                                </div>
+                                                <p className="text-xs font-thin text-red-600">Offline</p>
                                             </div>
-                                            <p className="text-xs font-thin text-red-600">Offline</p>
-                                        </div>
-                                        
-                                    )
-                                }
+                                            
+                                        )
+                                    }
+                                </div>
+                            </div> 
+                            <div className="flex gap-2 items-center">
+                                <button 
+                                    className="border-2 border-red-600 p-2 rounded-lg text-red-600 font-bold hover:bg-red-600 hover:text-white duration-300 flex gap-2"
+                                    onClick={() => handleDelete(friend.friendshipId)}
+                                    >
+                                    <p className="font-thin">Delete friend</p>
+                                    <AiOutlineUserDelete size={25}/>
+                                </button>
                             </div>
-                        </div> 
-                        <div className="flex gap-2 items-center">
-                            <button 
-                                className="border-2 border-red-600 p-2 rounded-lg text-red-600 font-bold hover:bg-red-600 hover:text-white duration-300 flex gap-2"
-                                onClick={() => handleDelete(friend.friendshipId)}
-                                >
-                                <p className="font-thin">Delete friend</p>
-                                <AiOutlineUserDelete size={25}/>
-                            </button>
+                            
+    
                         </div>
                         
-
-                    </div>
-                    
-                    {/* CHAT BOX */}
-                    <div className='w-full flex-1 flex flex-col gap-4 overflow-y-auto p-2 scrollbar-thin'>
-
-                        {messages && messages.map((message:any) => (
-
-                            <div key={message.messageId} className='flex flex-col'>
-
-                                {/* MY MESSAGE */}
-                                {Number(message.from_user) === Number(me_id) && 
-                                    <div className='rounded-xl border-2 border-black text-black w-1/2 self-start h-fit break-words'>
+                        {/* CHAT BOX */}
+                        <div className='w-full flex-1 flex flex-col gap-4 overflow-y-auto p-2 scrollbar-thin'>
+    
+                            {messages && messages.map((message:any) => (
+    
+                                <div key={message.messageId} className='flex flex-col'>
+    
+                                    {/* MY MESSAGE */}
+                                    {Number(message.from_user) === Number(me_id) && 
+                                        <div className='rounded-xl border-2 border-black text-black w-1/2 self-start h-fit break-words'>
+                                            <p className='p-3 w-full h-full text-left'>
+                                                {message.content}
+                                            </p>
+                                        </div>
+                                    }
+    
+                                    {/* OTHER PERSON MESSAGE */}
+                                    {Number(message.from_user) === Number(friend_id) && 
+                                        <div className='rounded-xl border-2 border-black text-white bg-black w-1/2 self-end'>
                                         <p className='p-3 w-full h-full text-left'>
                                             {message.content}
                                         </p>
                                     </div>
-                                }
-
-                                {/* OTHER PERSON MESSAGE */}
-                                {Number(message.from_user) === Number(friend_id) && 
-                                    <div className='rounded-xl border-2 border-black text-white bg-black w-1/2 self-end'>
-                                    <p className='p-3 w-full h-full text-left'>
-                                        {message.content}
-                                    </p>
+                                    }
+    
                                 </div>
-                                }
-
-                            </div>
-                        ))}
-
-                        <div ref={endOfChatBoxRef}/>
-
+                            ))}
+    
+                            <div ref={endOfChatBoxRef}/>
+    
+                        </div>
+    
+                        {/* MESSAGE AREA */}
+                        <form onSubmit={handleSubmit} className='flex w-full h-14'>
+                            
+                            <input 
+                                type="text" 
+                                name="message" 
+                                id="message" 
+                                className='border-t-2 border-black w-10/12 resize-none outline-none px-2 py-1 rounded-b-lg' 
+                                value={message} 
+                                onChange={(e) => setMessage(e.target.value)}
+                            />
+    
+                            <button 
+                            onClick={handleSubmit} 
+                            className='border-t-2 border-l-2 border-black hover:bg-black hover:text-white duration-300 px-2 cursor-pointer w-2/12 grid place-items-center'>
+                                <IoMdSend size={25}/>
+                            </button>
+    
+                        </form>
+    
                     </div>
-
-                    {/* MESSAGE AREA */}
-                    <form onSubmit={handleSubmit} className='flex w-full h-14'>
-                        
-                        <input 
-                            type="text" 
-                            name="message" 
-                            id="message" 
-                            className='border-t-2 border-black w-10/12 resize-none outline-none px-2 py-1 rounded-b-lg' 
-                            value={message} 
-                            onChange={(e) => setMessage(e.target.value)}
-                        />
-
-                        <button 
-                        onClick={handleSubmit} 
-                        className='border-t-2 border-l-2 border-black hover:bg-black hover:text-white duration-300 px-2 cursor-pointer w-2/12 grid place-items-center'>
-                            <IoMdSend size={25}/>
-                        </button>
-
-                    </form>
-
+                
                 </div>
             }
-        </div>
+        </>
+        
     )
 }
 
